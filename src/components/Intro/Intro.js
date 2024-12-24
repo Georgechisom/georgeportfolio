e4eb8ceda6842e0ced22e6cec1ee54d8d1f82db9
George winsome <georgechipaul@gmail.com>
2024-12-25 00:04:18 +0100

    portfolio updated

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -12,7 +12,7 @@ const Intro = () => {
           <span>I'm <span className='text-skyText'>George</span> <br />Web and Mobile Developer</span>
           <p className='text-lg lg:text-xl font-medium'>I am an experienced developer specializing in websites and mobile apps, <br /> adept at designing distinctive, exquisite, and user-friendly applications.</p>
           <Link className='decoration-0'>
-            <button className='flex items-center justify-center gap-2 md:gap-3 text-nowrap bg-whiteText my-4 mx-0 py-2 lg:py-3 px-4 lg:px-8 rounded-full border-2 border-skyText text-sm md:text-base lg:text-xl group text-skyText
+            <button className='flex items-center justify-center gap-2 md:gap-3 text-nowrap bg-whiteText my-4 mx-0 py-2 lg:py-3 px-4 lg:px-8 rounded-full border-b-2 border-r-2 border-skyText text-sm md:text-base lg:text-xl group text-skyText
             shadow-md shadow-skyText duration-300 hover:bg-skyText hover:text-whiteText hover:shadow-none'>
               <img src={intro} className='w-5 md:w-6 lg:w-7 object-cover' alt='introbuttonimage'/> Hire Me
             </button>
--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,8 +1,8 @@
 import React from 'react'
-// import { Link } from 'react-scroll';
-import cloth from "../../assets/clothweb.jpg";
 import product from "../../assets/productweb.jpg";
 import memes from "../../assets/meme.jpg";
+import form from "../../assets/multiform.jpg"
+import { Link } from 'react-scroll';
 
 // const Portfolio = [
 //     { title: "Paradise Comfort", image: `${cloth}`, link: "https://paradisecomfort.com.ng/" },
@@ -10,33 +10,45 @@ import memes from "../../assets/meme.jpg";
 //     { title: "Meme Generator", image: `${memes}`, link: "https://meme-generator-app-delta.vercel.app/"},
 //     { title: "George Portfolio", image: `${cloth}`, link: "https://georgechisom.vercel.app"},
 //     { title: "Multi Form", image: `${cloth}`, link: "https://relaxed-kangaroo-973393.netlify.app/"},
-//     { title: "Multi Form", image: `${cloth}`, link: "https://relaxed-kangaroo-973393.netlify.app/"}
+//     { title: "Multi Form", image: `${form}`, link: "https://relaxed-kangaroo-973393.netlify.app/"}
 // ];
-
-const Portfolio = [
-    { title: "George Luxury", image: `${product}`, link: "https://georgeluxury.vercel.app/"},
-    { title: "Meme Generator", image: `${memes}`, link: "https://meme-generator-app-delta.vercel.app/"},
-    { title: "Multi Form", image: `${cloth}`, link: "https://relaxed-kangaroo-973393.netlify.app/"},
-];
 
 const Works = () => {
   return (
-    <section className='max-w-screen-xl mx-auto w-full overflow-hidden flex flex-col items-center justify-center text-center'>
+    <section className='max-w-screen-xl mx-auto w-full overflow-hidden flex flex-col items-center justify-center text-center my-2'>
         <h2 className='text-2xl md:text-3xl lg:text-4xl font-extrabold py-6 md:py-10 mb-5'>My Portfolio</h2>
         <span className='font-medium text-sm sm:text-base py-0 px-6'>
             I derive great satisfaction from concentrating on the finest details and guaranteeing that my work is flawless down to the pixel. I am excited to utilizing my skills and experience to assist businesses achieve their goals and establish a robust online presence.
         </span>
-        <div className='grid lg:grid-cols-3 grid-cols-2 gap-4 lg:gap-2 my-10 px-4'>
+        {/* <div className='grid lg:grid-cols-3 grid-cols-2 gap-4 lg:gap-2 my-10 px-4'>
             {
                 Portfolio.map( ({ title, link, image }) => (
-                    <a key={title} href={link} className='border-2 border-skyText flex items-center justify-center rounded-lg py-3 px-3 data-[focus]:bg-white/50 tracking-wide hover:cursor-pointer'>
-                        <img src={image} alt='myWorks' className='object-cover rounded-md' />
+                    <a key={title} href={link} className='border-2 border-skyText flex flex-col gap-2 items-center justify-center rounded-lg py-3 px-3 data-[focus]:bg-white/50 tracking-wide hover:cursor-pointer'>
+                        <img src={image} alt='myWorks' className='object-cover rounded-md w-80' />
+                        <b className='text-sm md:text-base'>{title}</b>
                     </a>
                 )
 
                 )
             }
+        </div> */}
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 lg:gap-2 my-10 px-4'>
+            <Link to='https://georgeluxury.vercel.app/' className='border-2 border-skyText flex flex-col gap-2 items-center justify-center rounded-lg py-3 px-3 data-[focus]:bg-white/50 tracking-wide hover:cursor-pointer'>
+                <img src={product} alt="ecommerceImage" className='object-cover rounded-md w-80' />
+                <span className='text-sm md:text-base font-semibold'>Luxury Store</span>
+            </Link>
+            <Link to='https://meme-generator-app-delta.vercel.app/' className='hidden border-2 border-skyText lg:flex flex-col gap-2 items-center justify-center rounded-lg py-3 px-3 data-[focus]:bg-white/50 tracking-wide hover:cursor-pointer'>
+                <img src={memes} alt="memeGenerator" className='object-cover rounded-md w-72 lg:64' />
+                <span className='text-sm md:text-base font-semibold'>Meme Generator</span>
+            </Link>
+            <Link to='https://relaxed-kangaroo-973393.netlify.app/' className='border-2 border-skyText flex flex-col gap-2 items-center justify-center rounded-lg py-3 px-3 data-[focus]:bg-white/50 tracking-wide hover:cursor-pointer'>
+                <img src={form} alt="memeGenerator" className='object-cover rounded-md w-80' />
+                <span className='text-sm md:text-base font-semibold'>Multi~Step Form</span>
+            </Link>
         </div>
+        <button className='rounded-full border-b-2 border-r-2 border-t-0 shadow-sm shadow-skyText border-skyText py-3 px-8 my-4 font-semibold hover:bg-white hover:text-skyText hover:border-none hover:shadow-md hover:rounded-full hover:shadow-skyText hover:font-bold'>
+            See More
+        </button>
     </section>
   );
 }
