123a2321e4353909d2a23d99c5d361f153e2647d
George winsome <georgechipaul@gmail.com>
2025-01-02 18:09:13 +0100

    portfolio updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -36,9 +36,9 @@ const Contact = () => {
                 Kindly fill out the form provided below to discuss any business opportunities.
             </span>
             <form className='flex flex-col items-center justify-center gap-4 lg:gap-4 my-10 py-4'>
-                <input type='text' className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-md shadow-skyText border-skyText py-3 px-8 text-black rounded-full hover:border-2 hover:border-black hover:shadow-black hover:shadow-sm hover:bg-white focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Name' />
-                <input type='email' className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-md shadow-skyText border-skyText py-3 px-8 text-black rounded-full hover:border-2 hover:bg-white hover:border-black hover:shadow-black hover:shadow-sm focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Email' />
-                <textarea name='message' rows="5" className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-md shadow-skyText border-skyText py-3 px-8 text-black rounded-lg hover:border-2 hover:border-black hover:shadow-black hover:shadow-sm hover:bg-white focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Message'></textarea>
+                <input type='text' className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-lg shadow-skyText border-skyText py-3 px-8 text-black rounded-full hover:border-2 hover:border-black hover:shadow-black hover:shadow-sm hover:bg-white focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Name' />
+                <input type='email' className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-lg shadow-skyText border-skyText py-3 px-8 text-black rounded-full hover:border-2 hover:bg-white hover:border-black hover:shadow-black hover:shadow-sm focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Email' />
+                <textarea name='message' rows="5" className='font-medium w-full max-w-[40rem] m-2 border-t-0 border-b-2 border-r-2 shadow-lg shadow-skyText border-skyText py-3 px-8 text-black rounded-lg hover:border-2 hover:border-black hover:shadow-black hover:shadow-sm hover:bg-white focus:bg-white placeholder:text-black focus:border-none' placeholder='Your Message'></textarea>
                 <button type='submit' value="send" className=''>Submit</button>
             </form>
             <div>
