c37cc3bb75732c9e0e6d28a80b9159b4fbe3f603
George winsome <georgechipaul@gmail.com>
2025-01-04 16:33:19 +0100

    portfolio footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='max-w-screen-xl mx-auto w-full overflow-hidden flex items-center justify-center text-center my-6 font-semibold text-xs sm:text-sm md:text-base lg:text-lg'>
+    <footer className='max-w-screen-xl mx-auto w-full overflow-hidden flex items-center justify-center text-center my-6 font-semibold text-sm sm:text-sm md:text-base lg:text-lg'>
         copy &#169; 2025 George Chisom. All Rights Reserved.
     </footer>
   )
